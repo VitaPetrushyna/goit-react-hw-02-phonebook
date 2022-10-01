51ad61a63b362bdbd35bc973e254821345865e86
VitaPetrushyna <masandra2018@ukr.net>
2022-10-01 21:08:24 +0300

    task 1 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  // render() {
+  //   return (
+  //       <div>
+  //   <h1>Phonebook</h1>
+  //   <ContactForm ... />
+
+  //   <h2>Contacts</h2>
+  //   <Filter ... />
+  //   <ContactList ... />
+  // </div>
+
+  //     )
+  //   }
+}
